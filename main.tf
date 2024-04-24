ec4ac61871479b93079f54102017c05884186973
Pavani <vemasani.pavani@gmail.com>
2024-04-24 20:37:25 +0530

    added instance sopt options

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
 resource "aws_instance" "web" {
   ami = "ami-090252cbe067a9e58"
   instance_type = "t3.micro"
-
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      spot_instance_type = "Persistent"
+      instance_interruption_behavior = "stop"
+    }
+  }
 }
 resource "null_resource" "demo" {
   provisioner "remote-exec" {
