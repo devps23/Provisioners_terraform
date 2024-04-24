b58610b6b1f01fc46daa4b95bf17baf75e6e2840
Pavani <vemasani.pavani@gmail.com>
2024-04-24 18:09:54 +0530

    added connection inside provisoner

--- main.tf
+++ main.tf
@@ -1,14 +1,13 @@
 resource "aws_instance" "web" {
   ami = "ami-090252cbe067a9e58"
   instance_type = "t3.micro"
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    password = "DevOps321"
-    host     = self.public_ip
-  }
-
   provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
     inline = [
       "sudo dnf install nginx -y"
 
